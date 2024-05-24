4d13773012286b531578d2309a21750f82ea0a2f
TiagoBR24 <129324060+TiagoBR24@users.noreply.github.com>
2024-05-24 15:42:42 -0300

    aaa

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
+const text=document.querySelectorAll(".aba-conteudo"0;)
+
 for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
